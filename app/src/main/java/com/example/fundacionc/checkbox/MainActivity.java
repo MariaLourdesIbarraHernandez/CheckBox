f67709965a8a44f22922cab9dad20da0979034c8
maria <maria_abasolo@hotmail.com>
2018-04-17 17:01:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fundacionc/checkbox/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.fundacionc.checkbox;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    CheckBox chkBold, chkItalic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.txt);
+        chkBold = (CheckBox) findViewById(R.id.chkBold);
+        chkItalic = (CheckBox) findViewById(R.id.checkItalic);
+
+
+        chkBold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setTypeface(null, Typeface.NORMAL);
+
+                if (chkBold.isChecked() & chkItalic.isChecked())
+                    editText.setTypeface(null, Typeface.BOLD_ITALIC);
+
+                else if (chkItalic.isChecked())
+                    editText.setTypeface(null, Typeface.ITALIC);
+
+                else if (chkBold.isChecked())
+                    editText.setTypeface(null, Typeface.BOLD);
+
+
+            }
+        });
+
+        chkItalic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editText.setTypeface(null, Typeface.NORMAL);
+
+                if (chkItalic.isChecked() & chkBold.isChecked())
+                    editText.setTypeface(null, Typeface.BOLD_ITALIC);
+
+                else if (chkBold.isChecked())
+                    editText.setTypeface(null, Typeface.BOLD);
+
+                else if (chkItalic.isChecked())
+                    editText.setTypeface(null, Typeface.ITALIC);
+
+
+            }
+        });
+    }
+}
